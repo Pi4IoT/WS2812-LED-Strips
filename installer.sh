94f284b3627334ccdd2d4f63fa38ae7609f0213b
Pi4IoT <36192933+Pi4IoT@users.noreply.github.com>
2018-12-08 11:15:17 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,40 +1,40 @@
 #------------------------------------------------
 # Script for prepare and install WS281X Library--
-# Pi4IoT - 05.12.2018----------------------------
+# Pi4IoT - 08.12.2018----------------------------
 #------------------------------------------------
 
-printf "\n${yellow}Check and installing build-essential:${normal}\n"
+echo -e "\n\e[1;31mCheck and installing build-essential:\e[0m"
 sudo apt-get install build-essential 
 
-printf "\n${yellow}Check and installing python-dev:${normal}\n"
+echo -e "\n\e[1;31mCheck and installing python-dev:\e[0m"
 sudo apt-get install python-dev 
 
-printf "\n${yellow}Check and installing unzip:${normal}\n"
+echo -e "\n\e[1;31mCheck and installing unzip:\e[0m"
 sudo apt-get install unzip 
 
-printf "\n${yellow}Check and installing wget:${normal}\n"
+echo -e "\n\e[1;31mCheck and installing wget:\e[0m"
 sudo apt-get install wget 
 
-printf "\n${yellow}Check and installing scons:${normal}\n"
+echo -e "\n\e[1;31mCheck and installing scons:\e[0m"
 sudo apt-get install scons 
 
-printf "\n${yellow}Check and installing swig:${normal}\n"
+echo -e "\n\e[1;31mCheck and installing swig:\e[0m"
 sudo apt-get install swig
 
-printf "\n${yellow}Donwload rpi_ws281x:${normal}\n"
+echo -e "\n\e[1;31mDonwload rpi_ws281x:\e[0m"
 wget https://github.com/jgarff/rpi_ws281x/archive/master.zip 
 
-printf "\n${yellow}Unzip rpi_ws281x:${normal}\n"
+echo -e "\n\e[1;31mUnzip rpi_ws281x:\e[0m"
 unzip master.zip 
 
-printf "\n${yellow}Change folder to rpi_ws281x:${normal}\n"
+echo -e "\n\e[1;31mChange folder to rpi_ws281x:\e[0m"
 cd rpi_ws281x-master 
 
-printf "\n${yellow}Run scons:${normal}\n"
+echo -e "\n\e[1;31mRun scons:\e[0m"
 sudo scons 
 
-printf "\n${yellow}Change folder to python:${normal}\n"
+echo -e "\n\e[1;31mChange folder to python:\e[0m"
 cd python
- 
-printf "\n${yellow}Run setup:${normal}\n"
+
+echo -e "\n\e[1;31mRun setup:\e[0m"
 sudo python setup.py install
